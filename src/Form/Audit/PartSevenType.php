857d979d21411614183b5c83a8080f1bb1d91a66
PHDarthoux <pierrehenri.darthoux@campus-eni.fr>
2021-11-08 14:58:05 +0100

    test

--- src/Form/Audit/PartSevenType.php
+++ src/Form/Audit/PartSevenType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\File;
 
 class PartSevenType extends AbstractType
@@ -17,22 +18,25 @@ class PartSevenType extends AbstractType
             ->add('document', FileType::class, ['label' => 'Importer les documents ',
                 'mapped' => false,
                 'required' => false,
+                'multiple' => true,
                 'constraints' => [
-                    new File([
-                        'maxSize' => '2048k',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/png',
-                            'image/jpg',
-                            'application/pdf'
-                            // A COMPLETER SI BESOIN
+                    new All([
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '2048k',
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/png',
+                                    'image/jpg',
+                                    'application/pdf',
+                                    // A COMPLETER SI BESOIN
+                                'mimeTypesMessage' => "La taille ou le format ne correspond pas",
+                                ],
+                            ])
                         ],
-                        'mimeTypesMessage' => "La taille ou le format ne correspond pas",
-                    ])
-                ],
-                'multiple' => 'multiple',
-            ])
-        ;
+                    ]),
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
